c7acc30902650aa6ea08bfe45cc9881fdd7abf88
Anderson Silva <andssilva.23@gmail.com>
2022-02-28 14:02:43 -0400

    feat: :rocket: conteúdo: criando um método na classe user (enviar emails)

--- bloco_33/dia_1/conteudo/user.py
+++ bloco_33/dia_1/conteudo/user.py
@@ -8,11 +8,15 @@ class User:
         self.email = email
         self.password = password
 
+    def reset_password(self):
+        print("Envia email de reset de senha")
+
 # Para invocar o método construtor, a sintaxe é NomeDaClasse(parametro 1, parametro 2)
 # Repare que o parâmetro self foi pulado -- um detalhe do Python.
 
 
 meu_user = User("Valentino Trocatapa", "valentino@tinytoons.com", "Grana")
+meu_user.reset_password()
 print(meu_user)
 print(meu_user.name)
 print(meu_user.email)
